d0c6dac4c022ce90655f94496f8a7ce1415ff047
Jonathan Abourbih <jon.abourbih@gmail.com>
2017-02-19 16:57:23 +0000

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to nestory-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/nestory_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns nestory-clj.core-test
+  (:require [clojure.test :refer :all]
+            [nestory-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
