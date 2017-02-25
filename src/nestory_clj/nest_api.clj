b141bbbb115705e56cab32d78af0bb9c487063a8
Jonathan Abourbih <jon.abourbih@gmail.com>
2017-02-25 14:36:05 +0000

    Parse nest data into structure

---
+++ src/nestory_clj/nest_api.clj
@@ -0,0 +1,22 @@
+(ns nestory-clj.nest-api)
+
+(defn- thermostat-property [body property]
+  (-> body (get-in [:devices :thermostats]) vals first property))
+
+(defn- structure-property [body property]
+  (-> body :structures vals first property))
+
+(defn nest-data [body]
+  (let [extract-thermostat-data (partial thermostat-property body)
+        extract-structure-data (partial structure-property body)]
+    {:humidity (extract-thermostat-data :humidity)
+     :target-temperature (extract-thermostat-data :target-temperature-c)
+     :name (extract-thermostat-data :name)
+     :ambient-temperature (extract-thermostat-data :ambient-temperature-c)
+     :away-status (extract-structure-data :away)
+     :structure-id (extract-structure-data :structure-id)
+     :thermostat-id (extract-thermostat-data :device-id)
+     :has-leaf (extract-thermostat-data :has-leaf)
+     :heating-state (extract-thermostat-data :hvac-state)
+     :time-to-target (extract-thermostat-data :time-to-target)
+     :is-using-emergency-heat (extract-thermostat-data :is-using-emergency-heat)}))
---
+++ test/nestory_clj/nest_api_test.clj
@@ -0,0 +1,37 @@
+(ns nestory-clj.nest-api-test
+  (:require [cheshire.core :as json]
+            [camel-snake-kebab.core :refer [->kebab-case-keyword]]
+            [clojure.test :refer :all]
+            [nestory-clj.nest-api :refer :all]))
+
+(def sample-nest-api-response (-> "test/nestory_clj/nest-response.json" 
+                                  slurp
+                                  (json/parse-string ->kebab-case-keyword)))
+
+(def expected-structure {
+  :humidity 55
+  :target-temperature 21.5
+  :name "Hallway"
+  :ambient-temperature 20.5
+  :away-status "home"
+  :structure-id "c-SEshyQhB7ft2mluyF3otVjI8WUnxDJpNaAd1gcaLCgvx8ogw5m-g"
+  :thermostat-id "JZ-U65a4K6EKQmzsnTzFPbTE2K-T_Yco"
+  :has-leaf false
+  :heating-state "heating"
+  :time-to-target "~0"
+  :is-using-emergency-heat false})
+
+(deftest nest-api-extraction
+  (testing "can extract nest data from api"
+    (is (= {:humidity 55
+            :target-temperature 21.5
+            :name "Hallway"
+            :ambient-temperature 20.5
+            :away-status "home"
+            :structure-id "c-SEshyQhB7ft2mluyF3otVjI8WUnxDJpNaAd1gcaLCgvx8ogw5m-g"
+            :thermostat-id "JZ-U65a4K6EKQmzsnTzFPbTE2K-T_Yco"
+            :has-leaf false
+            :heating-state "heating"
+            :time-to-target "~0"
+            :is-using-emergency-heat false} (nest-data sample-nest-api-response)))))
+
